610615c8b73a5dcef210e78388ccb04551776fbd
Lee Wen Yao <peterleewenyao@gmail.com>
2018-06-12 17:05:49 +0800

    able to control paddle

--- project-1/script.js
+++ project-1/script.js
@@ -20,6 +20,9 @@
 // }
 //canvas.drawCircle();
 // the canvas
+var keystate = {};
+var upKey = 38;
+var downKey = 40;
 var canvas = document.getElementById('theCanvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -30,14 +33,14 @@ var c = canvas.getContext('2d');
 var net = {
 
   h : 30,
-  w : 10,
+  w : 5,
   x : canvas.width/2, //
   y : 0,
-  netGap : canvas.height/15,
+  netGap : canvas.height/10, //gap between each net
 
 //draw net function
   draw : function(){
-     for(this.y = 0; this.y < canvas.height; this.y += this.netGap){
+     for(this.y = 0; this.y < canvas.height; this.y += this.netGap){ // -this.y += this.netGap- create each net with a gap in between each other
       c.beginPath()
       c.fillRect(this.x, this.y+this.netGap, this.w, this.h)
   }
@@ -45,13 +48,13 @@ var net = {
 }
 
 //ball
-
 var spawnAreaOnX = canvas.width-50;
 var spawnAreaOnY = canvas.height-50;
 var ballX = (spawnAreaOnX/2);
 var ballY = (spawnAreaOnY/2);
 var ballSpeedX = /*((Math.random() - 0.5) * 7) */ (Math.random() < 0.5 ? -1 : 1) * 7; //Math.random() - 0.5 spawns the ball towards a random direction. * 7 is just the speed
 var ballSpeedY = /*((Math.random() - 0.5) * 7) */ (Math.random() < 0.5 ? -1 : 1) * 7; //Math.random() - 0.5 spawns the ball towards a random direction. * 7 is just the speed
+
 // ball object
 var ball = {
 
@@ -76,6 +79,9 @@ var ball = {
   this.x += this.dx; // makes ball move, along x axis, once it is created
   this.y += this.dy; // makes ball move, along y axis, once it is created
 
+//ball can bounce off top and bottom screen
+//ball can bounce off left and right screen, *** but not against the paddle ***
+
   if ((this.x + this.radius > canvas.width) || (this.x - this.radius < 0)) {
       this.dx = -this.dx;
     } else if(this.y + this.radius > canvas.height || this.y - this.radius < 0){
@@ -85,6 +91,47 @@ var ball = {
 }
 
 //}//end of ball object
+
+// player object
+  var player = {
+
+    x: canvas.width-20,
+    y: canvas.height/2,
+    height: 70,
+    width: 10,
+    moveUp: upKey, // arrow up keyCode is 38
+    moveDown: downKey, //downArrow's keyCode is 40
+    //use keyCode to represent a key on the keyboard. It tells the program what key has been pressed or released.
+    //a keyCode is returned to the program when the button has been pressed.
+
+    draw : function(){
+      c.beginPath();
+      c.fillRect(this.x, this.y, this.width, this.height);
+    },
+
+    move : function(){
+      // if up key is pressed,
+      //   move player up
+      // else if down key is pressed,
+      //   move player down
+
+          if(keystate[this.moveUp]){
+            this.y += 7;
+          } else if (keystate[this.moveDown]) {
+            this.y -= 7;
+          }
+        }
+      }
+      document.addEventListener("keydown", function(key){
+        //checks which key is pressed
+            keystate[key.keyCode] = true;
+      });
+
+
+      document.addEventListener("keyup", function(key){
+        //checks which key is released
+            delete keystate[key.keyCode];
+      });
 
  // ai object
   var ai = {
@@ -97,42 +144,35 @@ var ball = {
       c.beginPath();
       c.fillRect(this.x, this.y, this.width, this.height);
     }
-}
-  //  move : function(){
 
-  //  }
+   //  move : function(){
 
-  // }
-
-// player object
-  var player = {
-    x: canvas.width-20,
-    y: canvas.height/2,
-    height: 70,
-    width: 10,
-
-    draw : function(){
-      c.beginPath();
-      c.fillRect(this.x, this.y, this.width, this.height);
+   //  }
     }
-  }
-//   move : function(){
 
-//    }
-
-//  }
 
 //animation
-function animate(){
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight);
-  ball.draw();
-  ball.move();
-  ai.draw();
-  player.draw();
-  net.draw();
-}
-animate();
+  function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    ball.draw();
+    ball.move();
+    ai.draw();
+    player.draw();
+    player.move();
+    net.draw();
+  }
+  animate();
+
+
+//*** paddle cannot move yet ***
+//*** score system not started yet ***
+
+
+
+
+
+
 
 
 //player 1 is on the left, 2/AI right
