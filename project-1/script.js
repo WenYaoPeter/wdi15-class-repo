89564769074acacd64f014047e5027526d70583d
Lee Wen Yao <peterleewenyao@gmail.com>
2018-06-12 11:16:17 +0800

    created ai and player paddle

--- project-1/script.js
+++ project-1/script.js
@@ -1,13 +1,3 @@
-
-
-//style the canvas
-var canvas = document.getElementById('theCanvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var c = canvas.getContext('2d');
-
-
 //var spawnAreaOnX = (innerWidth-50);
 //var spawnAreaOnY = (innerHeight-50);
 // var x = Math.random() * spawnAreaOnX; //random coordinate, to spawn the ball on a random spot
@@ -29,26 +19,30 @@ var c = canvas.getContext('2d');
 //      c.stroke();
 // }
 //canvas.drawCircle();
+// the canvas
+var canvas = document.getElementById('theCanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// canvas draw ai and player
+var c = canvas.getContext('2d');
 
-
+//ball
 var spawnAreaOnX = innerWidth-50;
 var spawnAreaOnY = innerHeight-50;
-var ballX = Math.random() * spawnAreaOnX
-var ballY = Math.random() * spawnAreaOnY
-var balldx = (Math.random() - 0.5) * 30
-var balldy = (Math.random() - 0.5) * 30
+var ballX = (spawnAreaOnX/2);
+var ballY = (spawnAreaOnY/2);
+var balldx = (Math.random() - 0.5) * 30;
+var balldy = (Math.random() - 0.5) * 30;
 // ball object
 var ball = {
 
-  x: ballX,
-  y: ballY,
+  x : ballX,
+  y : ballY,
   dx : balldx,
   dy : balldy,
   radius : 5,
 
-//draw the ball, on x & y axis, random spot
+//draw the ball function, on x & y axis, random spot
   draw : function(){
   c.beginPath()
   c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
@@ -58,60 +52,68 @@ var ball = {
   c.stroke()
 },
 
-//move the ball
+//move the ball function
   move : function(){
-  this.x += this.dx;
-  this.y += this.dy;
+  this.x += this.dx; // makes ball move, along x axis, once it is created
+  this.y += this.dy; // makes ball move, along y axis, once it is created
 }
-
 }
+//   if((this.x + this.radius > aiPaddleWidth) || (this.x - this.radius < playerPaddleWidth)){
+//     this.x -= this.dx;
+//   } else if((this.y + this.radius > innerHeight) || (this.y - this.radius) > 0){
+//     this.y -= this.dy;
+//   }
+// }
+
+//}//end of ball object
 
 
+ // ai object
+  var ai = {
+    x: 20,
+    y: canvas.height/2,
+    height: 50,
+    width: 10,
 
+    draw : function(){
+      c.beginPath();
+      c.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+  //  move : function(){
+
+  //  }
+
+  // }
+
+// player object
+  var player = {
+    x: canvas.width-20,
+    y: canvas.height/2,
+    height: 50,
+    width: 10,
+
+    draw : function(){
+      c.beginPath();
+      c.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+//   move : function(){
+
+//    }
+
+//  }
+
+//animation
 function animate(){
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
   ball.draw();
   ball.move();
+  ai.draw();
+  player.draw();
 }
 animate();
-
-// player object
-// var player = {
-//   x: ,
-//   y: ,
-//   height: ,
-//   width: ,
-
-//   draw : function(){
-//     c.beginPath();
-//     c.fillRect(this.x, this.y, this.width, this.height)
-//   },
-
-//   move : function(){
-
-//   }
-
-// }
-// // ai object
-// var ai = {
-//   x: ,
-//   y: ,
-//   height: ,
-//   width: ,
-
-//   draw : function(){
-//     c.beginPath();
-//     c.fillRect(this.x, this.y, this.width, this.height)
-//   },
-
-//   move : function(){
-
-//   }
-
-// }
-
-
 
 
 //player 1 is on the left, 2/AI right
